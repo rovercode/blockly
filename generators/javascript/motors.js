b622ac51acd483a9c96325128beca5a88f515d24
Brady Hurlburt <brady.hurlburt@rovercode.com>
2020-04-23 00:13:14 -0400

    Update my email address

--- generators/javascript/motors.js
+++ generators/javascript/motors.js
@@ -21,7 +21,7 @@
 
 /**
  * @fileoverview Generating JavaScript for motor blocks.
- * @author brady.hurlburt@gmail.com (Brady L. Hurlburt)
+ * @author brady.hurlburt@rovercode.com (Brady L. Hurlburt)
  */
 'use strict';
 
