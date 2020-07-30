a413fded3c9fcfc81c0af6554e8ead5318b05beb
Brady L. Hurlburt <brady.hurlburt@rovercode.com>
2020-07-30 10:19:19 -0400

    Add line sensor

--- tests/rovercode-all-output.js
+++ tests/rovercode-all-output.js
@@ -6,6 +6,7 @@ setMotor('LEFT', 'FORWARD', '');
 stopMotor('LEFT');
 a = (getLightSensorValue('LEFT'));
 a = (buttonHasBeenPressed('A'));
+a = (getLineSensorValue('LEFT'));
 displayMessage(String('Sup'));
 displayMessage(String(123));
 displayMessage(String(true));
